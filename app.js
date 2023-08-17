23a75a4bc70b57941fdb0fed2249986a02fe8f8c
GhassanAbuKhaled <93045784+GhassanAbuKhaled@users.noreply.github.com>
2023-08-17 02:36:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-// Check if the user agent indicates a computer or iPad
-const isComputerOrIpad = /Macintosh|iPad/.test(navigator.userAgent);
-
-if (isComputerOrIpad) {
-    // Create an IntersectionObserver to track elements' visibility changes
-    const observer = new IntersectionObserver((entries) => {
-        // Iterate through each observed entry
-        entries.forEach((entry) => {
-            // If the element is currently intersecting the viewport
-            if (entry.isIntersecting) {
-                // Add the "show" class to reveal the element
-                entry.target.classList.add("show");
-            } else {
-                // Otherwise, remove the "show" class to hide the element
-                entry.target.classList.remove("show");
-            }
-        });
+// Create an IntersectionObserver to track elements' visibility changes
+const observer = new IntersectionObserver((entries) => {
+    // Iterate through each observed entry
+    entries.forEach((entry) => {
+        // If the element is currently intersecting the viewport
+        if (entry.isIntersecting) {
+            // Add the "show" class to reveal the element
+            entry.target.classList.add("show");
+        } else {
+            // Otherwise, remove the "show" class to hide the element
+            entry.target.classList.remove("show");
+        }
     });
-
-    // Select all elements with the "show" class
-    const showElements = document.querySelectorAll(".showLeft, .showRight , .showImg, .showLeft_slow, .showUp");
-
-    // Observe each show element for intersection changes
-    showElements.forEach((el) => observer.observe(el));
-}
+  });
+  
+  // Select all elements with the "show" class
+  const showElements = document.querySelectorAll(".showLeft, .showRight , .showImg, .showLeft_slow, .showUp");
+  
+  // Observe each show element for intersection changes
+  showElements.forEach((el) => observer.observe(el));
